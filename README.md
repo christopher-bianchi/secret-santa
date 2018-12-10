f73ea0e1077caa67cfa211ba4f4d532ac2d5605c
Christopher Bianchi <Christopher.Bianchi.10@gmail.com>
2018-12-09 22:04:32 -0500

    Add Docker Python script setup to run inside a Docker container. Supports env variables for email account secrets. Readme added for setup and run instructions.
    Have fun!

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # secret-santa
-A simple script for holiday fun.
+A simple script that automates matching and emailing for the Secret Santa holiday event.
+It takes a list of participants and for each santa participant will pair them with a giftee, notifying all participants of their match.
+
+- `santas.txt`: list of participants by email and name.
+- `email_template.txt`: the message that is sent to each participant.
+    - The email signoff name can be set inside the `docker-compose.yml`.
+- Credentials for the mailing account will need to be set.
+
+## Prerequisites
+- Install Docker: https://docs.docker.com/docker-for-mac/install/
+- A gmail account with permissions to send messages remotely.
+
+## Setup
+1. Clone the repo.
+2. Set local environment secrets for the email account that will be sending the event emails.
+    1. Open `~/.profile` for edit.
+    2. Add `SANTA_MAILER_ACCOUNT_EMAIL` to `~/.profile`:
+        - `export SANTA_MAILER_ACCOUNT_EMAIL=<mailer_email_address>`
+    3. Replace `<mailer_email_address>` with the email address that will be sending the event emails.
+    4. Add `SANTA_MAILER_ACCOUNT_TOKEN` to `~/.profile`:
+        - `export SANTA_MAILER_ACCOUNT_TOKEN=<mailer_app_token>`
+    5. Replace `<mailer_app_token>` with the password token for the email account.
+3. Reload profile to apply new env variables: `source ~/.profile`
+4. Open `docker-compose.yml`.
+5. Find `SANTA_EMAIL_SIGNOFF_NAME` under the `environment` section.
+6. Set the signoff name that will be used in the event emails by replacing `<signoff_name>`.
+    - e.g. `SANTA_EMAIL_SIGNOFF_NAME=Christopher`
+7. Save `docker-compose.yml`.
+8. Open `santas.txt`.
+9. Add the email addresses and names of all participants starting at line 2 (under the file header).
+    1. Remove the placeholder email examples at line 2 and line 3.
+    2. Add an email address, a space, then a name for the participant on one line.
+    3. Repeat on a new line for every participant until all event participants have been added to the `santas.txt` file.
+10. Save `santas.txt`.
+
+## Run it!
+1. `docker-compose run --rm secret-santa`
--- santa.py
+++ santa.py
@@ -1,48 +1,164 @@
+# coding:utf-8
+import csv
+import os
 import smtplib
+
+from collections import namedtuple
+from datetime import datetime
 from random import shuffle
-from pprint import pprint
-
-def get_email_template():
-    with open( "email.txt" ) as file:
-        email = file.read()
-    return email
-
-def create_santa_lists( santas, giftees ):
-    with open( "list.txt" ) as file:
-        for line in file:
-            santa_info = line.strip( '\n' ).split()
-            giftees.append( santa_info[1] )
-            santas.append( santa_info )
-
-def send_email( santa_email, santa, giftee ):
-    my_email = "myemail"
-    my_password = "mypass"
-
-    server = smtplib.SMTP( 'smtp.gmail.com', 587 )
-    server.starttls()
-    server.login( my_email, my_password )
-
-    email = get_email_template()
-    msg = email.format( santa, giftee )
-
-    server.sendmail( my_email, santa_email, msg )
-    server.quit()
 
 
-if __name__ == "__main__":
-    santas = list()
-    giftees = list()
-    create_santa_lists( santas, giftees )
-    shuffle( giftees )
-    for santas_info in santas:
-        found = False
-        i = 0
-        while not found:
-            if giftees[i] != santas_info[1]:
-                # email name
-                # print santas_info[0], santas_info[1], giftees[i]
-                send_email( santas_info[0], santas_info[1], giftees[i] )
-                del giftees[i]
-                found = True
-            else:
-                i += 1
+EMAIL_TEMPLATE = 'email_template.txt'
+SANTAS_FILE = 'santas.txt'
+MAILER_ACCOUNT_EMAIL = 'SANTA_MAILER_ACCOUNT_EMAIL'
+MAILER_ACCOUNT_TOKEN = 'SANTA_MAILER_ACCOUNT_TOKEN'
+MAILER_SIGNOFF_NAME = 'SANTA_EMAIL_SIGNOFF_NAME'
+
+SantaInfo = namedtuple('SantaInfo', ['email', 'name'])
+MailerCredentials = namedtuple('MailerCredentials', ['email', 'token'])
+
+
+class SecretSanta(object):
+    """
+    A fun, lightweight script to automate the selection process of the
+    Secret Santa holiday event.
+
+    Public:
+        kickoff(): entrypoint to match and email the secret santas.
+
+    Instance Attributes:
+        email:              stores the email template that is loaded from a
+                            file.
+        email_signoff_name: name used at bottom of email template for signoff
+                            signature.
+        mailer_creds:       a MailerCredentials namedtuple that stores the
+                            secrets for the email account that will deliver our
+                            event emails.
+        santas:             list(SantaInfo) - name and email of all
+                            participating santas.
+        giftees:            list(string) - names to be assigned to a santa.
+    """
+    def __init__(self):
+        self.email = self._load_email_template(EMAIL_TEMPLATE)
+        self.santas = []
+        self.giftees = []
+        self._load_settings_from_env()
+
+    def kickoff(self):
+        """
+        Fires off a round of matching all secret santas with a giftee then
+        notifies the santas by email.
+        """
+        self._create_santa_giftee_lists(SANTAS_FILE)
+        self._email_all_santas()
+
+    def _load_email_template(self, template):
+        """
+        Load email template from disk.
+
+        :param template: filename in path format.
+        """
+        print('Loading email template...')
+
+        with open(template) as file:
+            email = file.read()
+
+        return email
+
+    def _load_mailer_credentials_from_env(self):
+        """
+        Load the email account secrets that we will use to send our santa
+        emails from env variables.
+
+        :return MailerCredentials: tuple(email, token)
+        """
+        print('Loading email credentials from env...')
+
+        account_creds = {}
+        for key, value in os.environ.items():
+            if key == MAILER_ACCOUNT_EMAIL:
+                account_creds['email'] = value
+            if key == MAILER_ACCOUNT_TOKEN:
+                account_creds['token'] = value
+
+        return MailerCredentials(**account_creds)
+
+    def _load_settings_from_env(self):
+        """
+        Sets script settings from env variables.
+        """
+        self.mailer_creds = self._load_mailer_credentials_from_env()
+        print('Loading signoff name from env...')
+        self.email_signoff_name = os.environ.get(MAILER_SIGNOFF_NAME)
+
+    def _create_santa_giftee_lists(self, santas_list):
+        """
+        Loads the list of our santa participants from disk then generates a
+        list of secret santas. Also generates and shuffles a list of giftees
+        from the secret santas.
+
+        :param santas_list: filename in path format.
+        """
+        print('Loading santas and creating giftees...')
+        self.santas = []
+        self.giftees = []
+
+        with open(santas_list) as santa_file:
+            csv_reader = csv.DictReader(santa_file, delimiter=' ')
+
+            for santa_row in csv_reader:
+                santa_info = SantaInfo(**santa_row)
+
+                self.giftees.append( santa_info.name )
+                self.santas.append( santa_info )
+
+        shuffle(self.giftees)
+
+    def _send_email(self, conn, santa, giftee):
+        """
+        Send an email to inform our secret santa who they will be gifting.
+
+        :param conn: the connection to the email server.
+        :param santa: SantaInfo for the secret santa.
+        :param giftee: the name of the person the santa will gift.
+        """
+        print('Sending santa email for: {santa}'.format(santa=santa.name))
+
+        msg = self.email.format(
+            to_email=santa.email,
+            year=datetime.now().year,
+            santa=santa.name,
+            giftee=giftee,
+            signoff_name=self.email_signoff_name
+        )
+        conn.sendmail(self.mailer_creds.email, [santa.email], msg)
+
+    def _email_all_santas(self):
+        """
+        Opens an SMTP connection, logs in our mailer account and emails each
+        santa.
+        """
+        print('Logging into gmail account...')
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as es:
+            es.starttls()
+            es.login(self.mailer_creds.email, self.mailer_creds.token)
+
+            for santa_info in self.santas:
+                found = False
+                i = 0
+                while not found:
+                    # a santa cannot gift themself.
+                    giftee = self.giftees[i]
+                    if giftee != santa_info.name:
+                        found = True
+                        self._send_email(es, santa_info, giftee)
+                        # shrink list once a match is made.
+                        self.giftees.remove(giftee)
+                    else:
+                        i += 1
+
+
+if __name__ == '__main__':
+    santa = SecretSanta()
+    santa.kickoff()
